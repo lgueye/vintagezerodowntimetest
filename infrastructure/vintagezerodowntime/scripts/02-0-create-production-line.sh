f7cc80c43b636510f27aef2d942d565347edca5c
louis gueye <louis.gueye@gmail.com>
2017-06-11 15:31:08 +0200

    set production revision

--- infrastructure/vintagezerodowntime/scripts/02-0-create-production-line.sh
+++ infrastructure/vintagezerodowntime/scripts/02-0-create-production-line.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd ~/workspace/vintagezerodowntime-test/infrastructure && time ansible-playbook --vault-password-file ~/.vault_pass.txt -i ~/.digitalocean/vintagezerodowntime -e "rev=3fd4ecd target_env=production" vintagezerodowntime/create_line.yml
+cd ~/workspace/vintagezerodowntime-test/infrastructure && time ansible-playbook --vault-password-file ~/.vault_pass.txt -i ~/.digitalocean/vintagezerodowntime -e "rev=8c4a106 target_env=production" vintagezerodowntime/create_line.yml
 
