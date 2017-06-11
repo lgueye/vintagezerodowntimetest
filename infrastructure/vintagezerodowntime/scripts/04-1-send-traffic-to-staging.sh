5ee82bcaa66a1ccb5b54832f2d808c71deffddd1
louis gueye <louis.gueye@gmail.com>
2017-06-11 16:34:56 +0200

    set production revision

--- infrastructure/vintagezerodowntime/scripts/04-1-send-traffic-to-staging.sh
+++ infrastructure/vintagezerodowntime/scripts/04-1-send-traffic-to-staging.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd ~/workspace/vintagezerodowntime-test/infrastructure && time ansible-playbook --vault-password-file ~/.vault_pass.txt -i ~/.digitalocean/vintagezerodowntime -e "rev=a3da546 target_env=staging duration=20" vintagezerodowntime/send_traffic.yml
+cd ~/workspace/vintagezerodowntime-test/infrastructure && time ansible-playbook --vault-password-file ~/.vault_pass.txt -i ~/.digitalocean/vintagezerodowntime -e "rev=bd58cfb target_env=staging duration=20" vintagezerodowntime/send_traffic.yml
 
--- infrastructure/vintagezerodowntime/scripts/05-1-upgrade-production-line.sh
+++ infrastructure/vintagezerodowntime/scripts/05-1-upgrade-production-line.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cd ~/workspace/vintagezerodowntime-test/infrastructure && time ansible-playbook --vault-password-file ~/.vault_pass.txt -i ~/.digitalocean/vintagezerodowntime -e "rev=a3da546 target_env=production clean_db=false" vintagezerodowntime/upgrade_line.yml
+cd ~/workspace/vintagezerodowntime-test/infrastructure && time ansible-playbook --vault-password-file ~/.vault_pass.txt -i ~/.digitalocean/vintagezerodowntime -e "rev=bd58cfb target_env=production clean_db=false" vintagezerodowntime/upgrade_line.yml
